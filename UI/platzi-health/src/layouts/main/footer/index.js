6b3e8f678186a24dee62982b01322d90a471b8ee
Oriana <orianacmv@gmail.com>
2022-08-27 20:56:13 -0400

    ADD layout

---
+++ UI/platzi-health/src/layouts/main/footer/index.js
@@ -0,0 +1,3 @@
+const Footer = () => <p>footer</p>;
+
+export default Footer;
--- UI/platzi-health/src/pages/App/App.js
+++ UI/platzi-health/src/pages/App/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import { Web3ReactProvider } from '@web3-react/core'
+import { Web3ReactProvider } from '@web3-react/core';
 import { Web3Provider } from "@ethersproject/providers";
+import MainLayout from '../../layouts/main';
 
 function getLibrary(provider) {
   return new Web3Provider(provider);
@@ -9,7 +10,9 @@ function getLibrary(provider) {
 function App({ Component, pageProps }) {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-     <Component {...pageProps} />
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
     </Web3ReactProvider>
   );
 }
