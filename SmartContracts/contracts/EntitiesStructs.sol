94b64c9126f29d9c60d28cea2dcfb35bdd882783
Gabriel Rodriguez <51538222+gabarod@users.noreply.github.com>
2022-09-02 22:19:34 -0500

    Create EntitiesStructs.sol

---
+++ SmartContracts/contracts/EntitiesStructs.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract EntitiesStructs {
+    struct PersonalInformation {
+        uint128 id;
+        string name;
+        uint16 age;
+        string gender;
+        string email;
+        string countryCode;
+        string phoneNumber;
+    }
+
+    struct Patient {
+        PersonalInformation personalInformation;
+        bool isActive;
+        address[] doctorAccessList;
+    }
+
+    struct Doctor {
+        string specialization;
+        bool isActive;
+        PersonalInformation personalInformation;
+    }
+
+    struct Hospital {
+        uint128 id;
+        bool isActive;
+        string name;
+        string hospitalAddress;
+        string phoneNumber;
+    }
+
+    struct DoctorRegistration {
+        uint128 id;
+        string patientRecordId;
+    }
+
+    struct PatientRecord {
+        uint128 id;
+        bool isActive;
+        address doctorId;
+        address patientId;
+        string date;
+        string diagnosis;
+    }
+}
