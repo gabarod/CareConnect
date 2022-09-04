0fc250716d9f4e93d866ea4e19a683764acf7b74
Oriana <orianacmv@gmail.com>
2022-09-04 16:03:46 -0400

    ADD files to Records contract

--- SmartContracts/contracts/Records.sol
+++ SmartContracts/contracts/Records.sol
@@ -3,8 +3,9 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import {EntitiesStructs as es} from "./EntitiesStructs.sol";
+import './Files.sol';
 
-contract Records {
+contract Records is Files{
     // Mappings entities
     mapping(address => es.Doctor) Doctors;
     mapping(address => es.Patient) Patients;
@@ -227,5 +228,30 @@ contract Records {
         onlyHospital
     {
         hospitalToDoctorAccess[msg.sender] = doctor_id;
+    }
+
+    /**
+     * @dev add report files to a patient
+     */
+    function addFile(
+        address _patientAddress,
+        string memory _type,
+        string memory _hash
+    ) public {
+        //require(hasRole(DOCTOR_ROLE, msg.sender), "addPatientFile: solo doctores pueden agregar informes");
+        _addPatientFile(_patientAddress, _type, _hash);
+    }
+
+    /**
+     * @dev get report files to a patient
+     */
+    function getFiles(address _patientAddress)
+        public
+        view
+        returns (File[] memory)
+    {
+        //require(hasRole(DOCTOR_PATIENT, msg.sender), "getFiles: solo pacientes pueden ver informes");
+        //require(hasRole(DOCTOR_ROLE, msg.sender), "getFiles: solo doctores pueden ver informes");
+        return _getPatientFiles(_patientAddress);
     }
 }
