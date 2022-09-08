cae7ff37e55b0f9c1fb332cbfeef9f8f2776f70c
Oriana <orianacmv@gmail.com>
2022-09-07 23:40:13 -0400

    fix chain validation

--- UI/platzi-health/src/components/Wallet/index.js
+++ UI/platzi-health/src/components/Wallet/index.js
@@ -1,13 +1,16 @@
-import { useWeb3React } from '@web3-react/core';
+import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core';
 import { useEffect } from 'react';
 import { injected } from '../../utils/connector/connector';
 import { Button } from 'react-bootstrap';
 import useTruncatedAddress from '../../hooks/useTruncatedAddress';
 
 export default function Wallet() {
-  const { active, account, activate, deactivate } =
+  const { active, account, activate, deactivate, error } =
     useWeb3React();
   const truncatedAddress = useTruncatedAddress(account);
+
+  const isUnsupportedChain = error instanceof UnsupportedChainIdError;
+
 
   async function connect() {
     try {
@@ -48,8 +51,8 @@ export default function Wallet() {
           {truncatedAddress}
         </Button>
       ) : (
-        <Button onClick={connect} variant="primary">
-          Connect to MetaMask
+        <Button onClick={connect} variant="primary" disabled={isUnsupportedChain}>
+          {isUnsupportedChain ? "Red no soportada" : "Conectar"}
         </Button>
       )}
     </>
